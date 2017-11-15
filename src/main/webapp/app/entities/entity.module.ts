50d6914b3a9d55217f0a24365fbbebd0751056bd
dyngosz <jestemwiciu@gmail.com>
2017-11-15 18:17:05 +0100

    Weight entity

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -2,11 +2,13 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import {TwentyOnePointsPointsModule} from './points/points.module';
 
+import { TwentyOnePointsWeightModule } from './weight/weight.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         TwentyOnePointsPointsModule,
+        TwentyOnePointsWeightModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
